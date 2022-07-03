61c24989542b709424c2736308ab3ba263ee43db
binoy14 <binoy14@users.noreply.github.com>
2022-07-03 06:36:17 +0000

    deploy: 24dbaea1a303f7700f11deaa2d58e52683bd9451

--- _next/static/Tlm3da_HYNBU1HcroVB9g/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-566b41fcb3a11b1f.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/experience":["static/chunks/pages/experience-ac38c9110d3027cd.js"],sortedPages:["/","/_app","/_error","/experience"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
